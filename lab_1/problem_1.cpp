d3c37f26fa675b2b70beece3dc4fcb7b0801c26b
Vishesh-180101089 <50824297+korovishu@users.noreply.github.com>
2019-08-04 16:53:04 +0530

    Update problem_1.cpp

--- lab_1/problem_1.cpp
+++ lab_1/problem_1.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
+    int t;
+    cin >> t;
+    for(int i=0; i<t; i++)
+    {    
     string s1, s2, s3;
     cin >> s1 >> s2;
     int n1, n2;
@@ -24,5 +28,6 @@ int main() {
     }
     if(c!=0) s3.push_back(c + '0');
     reverse(s3.begin(), s3.end());
-    cout << s3;
+    cout << s3 << endl;
+    }    
 }
