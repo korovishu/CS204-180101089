86f0c3ffdc0c1a9a7a67ce11106fdf4dd3cda8a6
Vishesh-180101089 <50824297+korovishu@users.noreply.github.com>
2019-08-28 22:36:47 +0530

    Update ass.cpp

--- lab_5/ass.cpp
+++ lab_5/ass.cpp
@@ -1,6 +1,54 @@
 #include<bits/stdc++.h>
 using namespace std;
-unordered_map<string,int> chart;
+
+struct node
+{
+	string data;
+	string key;
+	node* next;
+};
+
+vector <struct node*> table;
+
+int calKey(string variable){
+	int val=0;
+	for(int i=0;i<variable.length();i++){
+		val+=variable[i]; 
+	}
+	return val%37;
+}
+
+void insert(string var,int val){
+	int ikey = calKey(var);
+	node* temp = new node();
+	temp->key = var;
+	temp->data = to_string(val);
+	temp->next = NULL;
+	if(table[ikey] == NULL){
+		table[ikey] = temp;
+	}
+	else{
+		node* temp2 = table[ikey];
+		temp->next = temp2;
+		table[ikey] = temp;
+	}
+
+}
+
+node* search(string p){
+	int ikey = calKey(p);
+	node* temp = table[ikey];
+	while(temp!=NULL){
+		if(!p.compare(temp->key))
+			break;
+	}
+	return temp;	
+}
+
+string integer(string p){
+	node* ptr = search(p);
+	return ptr->data;
+}
 
 struct et 
 { 
@@ -10,6 +58,8 @@ struct et
 
 int prec(string c) 
 { 
+	if(c=="@")
+	return 4;
     if(c == "^") 
     return 3; 
     else if(c == "*" || c == "/") 
@@ -18,16 +68,89 @@ int prec(string c)
     return 1; 
     else
     return -1; 
-} 
+}  
+vector<string> infixToPostfix(vector<string> s) 
+{ 
+    std::stack<string> st; 
+    st.push("N"); 
+    int l = s.size(); 
+    vector<string> ns; 
+    for(int i = 0; i < l; i++) 
+    { 
+        // If the scanned character is an operand, add it to output string. 
+        if(s[i]!="(" && s[i]!=")" && s[i]!="+" && s[i]!="*" && s[i]!="-" && s[i]!="/" && s[i]!= "@") {
+        	if(s[i]!="")
+        	ns.push_back(s[i]); 
+    	}
+  
+        // If the scanned character is an ‘(‘, push it to the stack. 
+        else if(s[i] == "(") 
+          
+        st.push("("); 
+          
+        // If the scanned character is an ‘)’, pop and to output string from the stack 
+        // until an ‘(‘ is encountered. 
+        else if(s[i] == ")") 
+        { 
+            while(st.top() != "N" && st.top() != "(") 
+            { 
+                string c = st.top(); 
+                st.pop();
+                ns.push_back(c); 
+            } 
+            if(st.top() == "(") 
+            { 
+                st.pop();  
+            } 
+        } 
+          
+        //If an operator is scanned 
+        else{ 
+            while(st.top() != "N" && prec(s[i]) <= prec(st.top())) 
+            { 
+                string c = st.top(); 
+                st.pop(); 
+                ns.push_back(c); 
+            } 
+            st.push(s[i]); 
+        } 
+  
+    } 
+    //Pop all the remaining elements from the stack 
+    while(st.top() != "N") 
+    { 
+                string c = st.top(); 
+                st.pop();
+                ns.push_back(c); 
+    } 
+      
+    return ns;
+  
+}  
 
 bool isOperator(string c) 
 { 
-    if (c.length()==1 && (c == "+" || c == "-" || 
+    if (c.size()==1&&(c == "+" || c == "-" || 
             c == "*" || c == "/" || 
-            c == "^") )
+            c == "^"|| c=="@")) 
         return true; 
     return false; 
 } 
+
+bool isOperator(char c) 
+{ 
+    if ((c == '+' || c == '-' || 
+            c == '*' || c == '/' || 
+            c == '^')) 
+        return true; 
+    return false; 
+} 
+
+bool isunary(string p,int i){
+	if(i==0||p[i+1]==28||isOperator(p[i-1]))
+		return true;	
+	return false;
+}
 
 et* newNode(string v) 
 { 
@@ -37,219 +160,156 @@ et* newNode(string v)
     return temp; 
 }; 
   
-
-vector<string> infixToPostfix(vector<string> s) 
-{ 
-    stack<string> st; 
-    st.push("N"); 
-    int l = s.size(); 
-    vector<string> ns; 
-    for(int i = 0; i < l; i++) 
-    { 
-         
-        if(!isOperator(s[i]) && s[i]!="" && s[i]!="(" && s[i]!=")") 
-         ns.push_back(s[i]); 
-        
-        else if(s[i] == "(")   
-        st.push("("); 
-          
-        else if(s[i] == ")") 
-        { 
-            while(st.top() != "N" && st.top() != "(") 
-            { 
-                string c = st.top(); 
-                st.pop(); 
-                
-                
-            if(c!="" && c!="(" && c!=")")ns.push_back(c); 
-              
-               
-            } 
-            if(st.top() == "(") 
-            { 
-                st.pop();  
-            } 
-        } 
-           
-        else{ 
-            while(st.top() != "N" && prec(s[i]) <= prec(st.top())) 
-            { 
-                if(s[i]=="^" && st.top()=="^") break;
-                string c = st.top(); 
-                st.pop(); 
-                
-                if(c!="" && c!="(" && c!=")")ns.push_back(c); 
-                
-            } 
-            st.push(s[i]); 
-        } 
-  
-    } 
-    while(st.top() != "N") 
-    { 
-                string c = st.top(); 
-                st.pop(); 
-                if(c!="" && c!="(" && c!=")")ns.push_back(c);  
-              
-    } 
-      
-    return ns;
-  
-} 
- 
+// Returns root of constructed tree for given 
+// postfix expression 
 et* constructTree(vector<string> postfix) 
 { 
     stack<et *> st; 
     et *t, *t1, *t2; 
   
+    // Traverse through every character of 
+    // input expression 
     for (int i=0; i<postfix.size(); i++) 
     { 
+        // If operand, simply push into stack 
         if (!isOperator(postfix[i])) 
         { 
             t = newNode(postfix[i]); 
             st.push(t); 
         } 
-        else 
+        else if(postfix[i] == "@"){
+        	string a = "0";
+        	t = newNode(postfix[i]);
+        	t1 = st.top();
+        	st.pop();
+        	t->right = t1;
+        	t2  = newNode(a);
+        	t->left = t2;
+        	st.push(t);
+        }
+        else // operator 
         { 
             t = newNode(postfix[i]); 
-  
-            t1 = st.top(); 
-            st.pop();      
+            // Pop two top nodes 
+            t1 = st.top(); // Store top 
+            st.pop();      // Remove top 
             t2 = st.top(); 
             st.pop(); 
-  
+            //  make them children 
             t->right = t1; 
             t->left = t2; 
-  
+            // Add this subexpression to stack 
             st.push(t); 
         } 
     } 
-   
+    //  only element will be root of expression 
+    // tree 
     t = st.top(); 
     st.pop(); 
-  
     return t; 
 } 
-int eval(et * root){
-    if(root==NULL) return 0;
-    if(root->left==NULL && root->right==NULL)
-    {
-        try { return stoi(root->value);
-        }
-       catch(...){
-        if(chart.find(root->value)!=chart.end()) return chart[root->value];
-        else 
-        { return INT_MAX;}
-        
-     }
+
+int eval(et* a){
+    if(a->left==NULL&&a->right==NULL){
+    	try{
+    		return stoi(a->value);
+    	}
+    	catch(...){
+    		return stoi(integer(a->value));
+    	}
     }
-    int l=eval(root->left);
-    int r=eval(root->right);
-    if(l==INT_MAX || r==INT_MAX) return INT_MAX;
-    if(root->value=="+") return l+r;
-    else if(root->value=="-") return l-r;
-    else if(root->value=="*") return l*r;
-    else if(root->value=="/") return l/r;
-    else return pow(l,r);
-}
-vector<string> goodstring(string s)
-{
-    int flag=0;
-         int mark=0;
-         string temp="";
-         vector<string> str;
-    for(int i=0; i<s.length();i++)
-    {
-        if( (s[i]>=48 && s[i]<=57) || (s[i]>=65 && s[i]<=90) || (s[i]>=97 && s[i]<=122) )
-        {
-            temp+=s[i];
-        }
-        
-        else
-        {
-            if((temp=="" && s[i]=='-' && i==0) || (temp=="" && s[i]=='-' && s[i-1]!=')' ) ) //check if unary minus is thee
-                {
-                    str.push_back("("); 
-                    str.push_back("0"); 
-                    str.push_back("-"); 
-                    if(s[i+1]=='(') flag+=1; //expression has minus on it
-                    else mark+=1; //just a number has minus on it
-                }
-            else{
-                  if(temp!="") str.push_back(temp);  //to push number or variablke formed 
-                  
-                  while(mark>0) { 
-                      str.push_back(")"); // close as many unary minus were opened
-                      mark--;
-                  }
-                  
-                  temp=""; temp+=s[i]; //to convert the operator or bracket  to string 
-                  if(temp!="") str.push_back(temp); // then push to vstring
-                  
-                  if(temp==")") { //as exp ends put the bracket close
-                      if(flag--)
-                         str.push_back(")"); 
-                  }
-                }
-                temp=""; //renew tmp
-        }
-    }
-    
-    str.push_back(temp);
-    if(mark>0) str.push_back(")");
-    return str;
+    int A = eval(a->left);
+    int B = eval(a->right);
+    if(a->value == "+")
+        return A+B;
+    else if(a->value == "-")
+        return A-B;
+    else if(a->value == "*")
+        return A*B;
+    else if(a->value == "%")
+        return A%B;
+    else if(a->value == "/")
+        return A/B;
+    else if(a->value == "^")
+        return pow(A,B);
+    else if(a->value == "@")
+    	return -1*B;
 }
 
-int main()
-{
-    int t;cin>>t;
-    while(t--)
-    {
-        int n;cin>>n;
-        chart.clear();
-      while(n--)
-      {
-         string s;
-         cin>>s;
-         vector<string> str;
-         string temp="";
-         
-         if(s.find("=") == std::string::npos )
-         {
-    
-          vector<string> str= goodstring(s);
-          vector<string> postexp=infixToPostfix(str);
+vector <string> Thevals(string p){
+    vector<string> P;
+    string temp="";
+	for(int i=0; i<p.length();i++)
+	{
+		if((p[i]>=48 && p[i]<=57))
+		{
+          temp+=p[i];
+		}
+		else
+		{
+            P.push_back(temp);
+            temp="";
+            if(p[i] == 45 && isunary(p,i))
+            	p[i] = '@';
 
-         struct et* root=constructTree(postexp);
-         int ans = eval(root);
-         if(ans != INT_MAX)
-         cout<<ans<<endl;
-         else cout<<"CANT BE EVALUATED"<<endl;
-         }
+            temp+=p[i];  
+            P.push_back(temp);
+            temp="";
+		}   
+	}
+    P.push_back(temp);
+    return P;
+}
 
-         else
-         {
-             int k= s.find("=");
-             string s1=s.substr(0,k);
-             string s2=s.substr(k+1);
-             vector<string> str= goodstring(s2);
-           vector<string> postexp=infixToPostfix(str);
+int numerical_exp(string p){
+	vector<string> P;
+	P = Thevals(p);
+	vector<string> postexp=infixToPostfix(P);
+    //for(int i=0; i<postexp.size();i++)
+    //cout<<postexp[i]<<" ";
+    et* a = constructTree(postexp);
+    int value = eval(a);
+    return value;
+}
 
-          struct et* root=constructTree(postexp);
-         int ans = eval(root);
-         if(chart.find(s1)!= chart.end() )
-         {
-          if(ans != INT_MAX)
-          chart[s1]=ans;
-          else cout<<"CANT BE EVALUATED"<<endl;
-         }
-         else
-         {
-           if(ans != INT_MAX)
-           chart.insert(make_pair(s1,ans));
-           else cout<<"CANT BE EVALUATED"<<endl;
-          }
-        }
-        } 
+void variable_exp(string p){
+	string var = "";
+	int i;
+	for(i=0;i<p.length();i++){
+		if(p[i] == 61)
+			break;
+		else
+			var+=p[i];
+	}
+	int num = numerical_exp(p.substr(i,p.length()));
+	insert(var,num);
+}
+void exptype(string p){
+	int flag = 0;
+	for(int i=0;i<p.length();i++){
+		if(p[i] == '='){
+			flag = 1;
+			break;
+		}
+	}
+	if(flag){
+        variable_exp(p);
     }
+    else{
+    	int number = numerical_exp(p);
+    	cout<<number<<"\n";
+    }
+}
+
+
+int main(){
+	int q=2;
+	for(int i=0;i<37;i++){
+			table.push_back(NULL);
+		}
+	while(q--){
+		string p;
+		cin>>p;
+		exptype(p);
+	}
 }
