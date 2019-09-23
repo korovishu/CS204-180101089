88cc6b89b1ffa46f506370b41b41c0c5533d05ea
Vishesh-180101089 <50824297+korovishu@users.noreply.github.com>
2019-09-23 16:43:48 +0530

    Update circle_radius.cpp

--- lab_postmidsem_1/circle_radius.cpp
+++ lab_postmidsem_1/circle_radius.cpp
@@ -2,22 +2,22 @@
 using namespace std;
 #define ll long long
 
-int partition(int arr[], int l, int r, int k); 
+ll partition(ll arr[], ll l, ll r, ll k); 
   
-int findMedian(int arr[], int n) 
+ll findMedian(ll arr[], ll n) 
 { 
     sort(arr, arr+n);   
     return arr[n/2];   
 } 
   
-int kthSmallest(int arr[], int l, int r, int k) 
+ll kthSmallest(ll arr[], ll l, ll r, ll k) 
 { 
     
     if (k > 0 && k <= r - l + 1) 
     { 
-        int n = r-l+1; 
+        ll n = r-l+1; 
    
-        int i,median[(n+4)/5]; 
+        ll i,median[(n+4)/5]; 
         for (i=0; i<n/5; i++) 
             median[i] = findMedian(arr+l+i*5, 5); 
         if (i*5 < n)  
@@ -26,10 +26,10 @@ int kthSmallest(int arr[], int l, int r, int k)
             i++; 
         }     
    
-        int medOfMed = (i == 1)? median[i-1]: 
+        ll medOfMed = (i == 1)? median[i-1]: 
                                  kthSmallest(median, 0, i-1, i/2); 
    
-        int pos = partition(arr, l, r, medOfMed); 
+        ll pos = partition(arr, l, r, medOfMed); 
   
         if (pos-l == k-1) 
             return arr[pos]; 
@@ -42,33 +42,33 @@ int kthSmallest(int arr[], int l, int r, int k)
     return INT_MAX; 
 } 
   
-void swap(int *a, int *b) 
+void swapm(ll *a, ll *b) 
 { 
-    int temp = *a; 
+    ll temp = *a; 
     *a = *b; 
     *b = temp; 
 } 
   
-int partition(int arr[], int l, int r, int x) 
+ll partition(ll arr[], ll l, ll r, ll x) 
 { 
     // Search for x in arr[l..r] and move it to end 
-    int i; 
+    ll i; 
     for (i=l; i<r; i++) 
         if (arr[i] == x) 
            break; 
-    swap(&arr[i], &arr[r]); 
+    swapm(&arr[i], &arr[r]); 
   
     // Standard partition algorithm 
     i = l; 
-    for (int j = l; j <= r - 1; j++) 
+    for (ll j = l; j <= r - 1; j++) 
     { 
         if (arr[j] <= x) 
         { 
-            swap(&arr[i], &arr[j]); 
+            swapm(&arr[i], &arr[j]); 
             i++; 
         } 
     } 
-    swap(&arr[i], &arr[r]); 
+    swapm(&arr[i], &arr[r]); 
     return i; 
 } 
 
@@ -77,17 +77,17 @@ int main()
     ll t; cin>>t;
     while(t--)
     {
-        int n;cin>>n;
-        int d[n];
-        for(int i=0; i<n; i++)
+        ll n;cin>>n;
+        ll d[n];
+        for(ll i=0; i<n; i++)
         {
-            int x,y; cin>>x>>y;
-            int t=(x*x+y*y);
+            ll x,y; cin>>x>>y;
+            ll t=(x*x+y*y);
             d[i]=t;
         }
-        int a=kthSmallest(d, 0, n-1, n/2);
+        ll a=kthSmallest(d, 0, n-1, n/2);
         double sa=sqrt(a);
-        int b=kthSmallest(d, 0, n-1, (n+1)/2);
+        ll b=kthSmallest(d, 0, n-1, (n+1)/2);
         double ba=sqrt(b);
         if(n%2==0) cout<<sa<<endl;
         else cout<<ba<<endl;
