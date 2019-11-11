0a7f5b96657646750ca865ec416ea40ad1852bed
Vishesh-180101089 <50824297+korovishu@users.noreply.github.com>
2019-11-11 20:14:22 +0530

    Update probab.cpp

--- lab6_postmidsem/probab.cpp
+++ lab6_postmidsem/probab.cpp
@@ -5,10 +5,6 @@ int k1 = 24379;
 int k2 = 23293;
 int k3 = 28687;
 
-int p1 = 18143;
-int p2 = 20749;
-int p3 = 17419;
-
 bool hashfn1[24379];
 bool hashfn2[23293];
 bool hashfn3[28687];
@@ -21,9 +17,9 @@ bool Hash(string s, int f)
 
 	for (int i=0; i<len; i++)
 	{
-		f1 = (f1 * p1 + (s[i]-'a')) % k1 ; 
-		f2 = (f2 * p2 + (s[i]-'a')) % k2 ; 
-		f3 = (f3 * p3 + (s[i]-'a')) % k3 ;  
+		f1 = (f1 + (i+1)*(i+1)*(s[i]-'a')) % k1 ; 
+		f2 = (f2 + (i+1)*(i+1)*(s[i]-'a')) % k2 ; 
+		f3 = (f3 + (i+1)*(i+1)*(s[i]-'a')) % k3 ;  
 	}
 
 	if (hashfn1[f1])
